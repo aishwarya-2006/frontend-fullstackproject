8c2b32e31c5e52243cb0210afe98fa824bdfc4fd
aishwarya <aishwaryakmm3@gmail.com>
2026-04-28 12:06:43 +0530

    property-value-enhancer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: "/urban-renovte/",
-  plugins: [react()],
-})
+export default {
+  base: "/property-value-enhancer/"
+}
